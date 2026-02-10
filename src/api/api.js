dbbb50fcf027dc30d47141e8066bdf9868971950
Joso1207 <joso1207@student.miun.se>
2026-02-10 07:44:23 +0100

    Fetch API

---
+++ src/api/api.js
@@ -0,0 +1,33 @@
+const API_URL = "https://swapi.info/api/";
+
+export async function fetchRequest(endpoint) {
+  try {
+    const result = await apiRequest(API_URL + endpoint);
+    localStorage.setItem(endpoint, JSON.stringify(result));
+    return result;
+
+  } catch (error) {
+    if (error instanceof TypeError) {
+      console.error("Failed to fetch ${endpoint}:", error);
+      return JSON.parse(localStorage.getItem(endpoint)) || [];
+    }
+    else {
+      throw error;
+    }
+  }
+}
+
+export async function apiRequest(requestUrl, options = {}) {
+  const response = await fetch(requestUrl, options);
+
+  if (!response.ok) {
+    throw new Error(`API error: ${response.status}`);
+  }
+  try {
+    const responseData = await response.json();
+    return responseData;
+
+  } catch (error) {
+    throw error;
+  }
+}
