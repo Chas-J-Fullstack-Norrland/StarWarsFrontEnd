87656d6afbf159ea72ab8f020cd923e3e5e08bdf
Joso1207 <joso1207@student.miun.se>
2026-02-24 18:43:47 +0100

    Test for View.js

--- src/view.js
+++ src/view.js
@@ -51,7 +51,7 @@ function mapItem(data,category){
             break;
         default:
             console.warn(`Unknown item type: ${category}`);
-            nameHeader.innerText = "No item" // Fallback
+            nameHeader.innerText = "Failed to load data." // Fallback
         }
 }
 
@@ -80,6 +80,8 @@ async function formatPeopleData(item) {
         <li>Gender: ${item.gender}</li>
         <li>Homeworld: ${homeworldLink}</li>
     `;
+
+
 }
 
 function formatPlanetsData(item){
