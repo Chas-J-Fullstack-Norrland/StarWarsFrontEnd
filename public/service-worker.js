f15226aa52f4e546eac271df00a8602d71b48e6a
Joso1207 <joso1207@student.miun.se>
2026-02-26 02:06:18 +0100

    Install and activate events

--- public/service-worker.js
+++ public/service-worker.js
@@ -17,3 +17,19 @@ const cachedAssets = [
     basePath + 'styles/main.css',
     basePath + 'styles/icon-192.png',
 ]
+
+self.addEventListener('install',event=>{
+    event.waitUntil(
+        caches.open(cacheName).then(cache=>cache.addAll(cachedAssets))
+        .then(()=> self.skipwaiting())
+    )
+});
+
+self.addEventListener('activate',event=>{
+    event.waitUntil(
+        caches.keys().then(keys=> Promise.all(keys.filter(
+            key => key !== cacheName).map(key=>caches.delete(key))
+        ).then(()=>self.clients.claim())
+        )
+    );
+});
