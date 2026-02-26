9ece96f216e995a345e71229f1b9bc0dc632eba1
Joso1207 <joso1207@student.miun.se>
2026-02-26 02:05:50 +0100

    Assets to precache

---
+++ public/service-worker.js
@@ -0,0 +1,19 @@
+const cacheName = 'Chas_Swapi-Cache-v1';
+const basePath = self.registration.scope;
+const cachedAssets = [
+    basePath,
+    basePath + 'index.html',
+    basePath + 'favorites.html',
+    basePath + 'list.html',
+    basePath + 'view.html',
+    basePath + 'favorites.js',
+    basePath + 'favoritesStorage.js',
+    basePath + 'list.js',
+    basePath + 'view.js',
+    basePath + 'main/icon-192.png',
+    basePath + 'icons/icon-192.png',
+    basePath + 'icons/icon-512.png',
+    basePath + 'icons/icon.svg',
+    basePath + 'styles/main.css',
+    basePath + 'styles/icon-192.png',
+]
