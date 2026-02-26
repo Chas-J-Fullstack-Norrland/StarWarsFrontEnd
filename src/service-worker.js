c4c63ed19b32ee4962232ef5ab466fd343476c28
Joso1207 <joso1207@student.miun.se>
2026-02-26 03:03:45 +0100

    relocated service worker to allow vite-pwa to build from our SW instead of copying it as is

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,43 +1,13 @@
+import {precacheAndRoute} from 'workbox-precaching';
+
+precacheAndRoute(self.__WB_MANIFEST || []);
+
 const cacheName = 'Chas_Swapi-Cache-v1';
-const basePath = self.registration.scope;
-const cachedAssets = [
-    basePath,
-    basePath + 'index.html',
-    basePath + 'favorites.html',
-    basePath + 'list.html',
-    basePath + 'view.html',
-    basePath + 'favorites.js',
-    basePath + 'favoritesStorage.js',
-    basePath + 'list.js',
-    basePath + 'view.js',
-    basePath + 'main/icon-192.png',
-    basePath + 'icons/icon-192.png',
-    basePath + 'icons/icon-512.png',
-    basePath + 'icons/icon.svg',
-    basePath + 'styles/main.css',
-    basePath + 'styles/icon-192.png',
-]
-
-self.addEventListener('install',event=>{
-    event.waitUntil(
-        caches.open(cacheName).then(cache=>cache.addAll(cachedAssets))
-        .then(()=> self.skipwaiting())
-    )
-});
-
-self.addEventListener('activate',event=>{
-    event.waitUntil(
-        caches.keys().then(keys=> Promise.all(keys.filter(
-            key => key !== cacheName).map(key=>caches.delete(key))
-        ).then(()=>self.clients.claim())
-        )
-    );
-});
 
 self.addEventListener('fetch',event  => {
     const request = event.request;
 
-    if(request.url.includes("api") && request.url.startsWith(basePath)){
+    if(request.url.endsWith("api/api.js")){
         event.respondWith(
             caches.match(request).then(cached=>{
                 if(cached) return cached;
