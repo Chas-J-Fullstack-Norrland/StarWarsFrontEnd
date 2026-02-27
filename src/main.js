4ee09f8223b089f85b73db06199f8119f7e6d1d6
Joso1207 <joso1207@student.miun.se>
2026-02-27 18:57:07 +0100

    Index page is now a quick presentation of the project

--- src/main.js
+++ src/main.js
@@ -4,9 +4,9 @@ import './header-footer.js'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 
-let app = document.querySelector('#app');
-if (app){
-  app.innerHTML = `
+let logoElement = document.querySelector('.logos');
+if (logoElement){
+  logoElement.innerHTML = `
     <div>
       <a href="https://vite.dev" target="_blank">
         <img src="${viteLogo}" class="logo" alt="Vite logo" />
@@ -14,11 +14,6 @@ if (app){
       <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
         <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
       </a>
-      <h1>Hello Vite!</h1>
-      <p class="read-the-docs">
-        Click on the Vite logo to learn more
-      </p>
-    </div>
   `;
 }
 
