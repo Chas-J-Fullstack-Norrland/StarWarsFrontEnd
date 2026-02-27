fd38c3db5461eee700b6d8eec330d29bf003a49e
Joso1207 <joso1207@student.miun.se>
2026-02-27 12:11:00 +0100

    Added updateConnected in headerfooter.js,  tied to online/offline/load events

--- src/header-footer.js
+++ src/header-footer.js
@@ -6,10 +6,10 @@ window.addEventListener("load", () => {
     document.head.insertAdjacentHTML("beforeend", `<link rel="stylesheet" href="./styles/header-footer.css">`);
     
     header.innerHTML = `
-        <section id="user-section">
+        <section id="header-logo-section">
           <a href="#" class="logo">Star Wars API</a>
           
-          <span id="status">Online</span>
+          <span id="status">ONLINE</span>
       </section>
 
       <nav id="category_nav">
@@ -26,4 +26,29 @@ window.addEventListener("load", () => {
     footer.innerHTML = `
         <p>Chas Academy - Norrland 2026</p>`;
 
+    statusIcon = document.querySelector("#status");
+    updateConnected(); 
+
 });
+
+let statusIcon;
+
+function updateConnected() {
+    if (!statusIcon) return;
+
+    if (navigator.onLine) {
+        statusIcon.textContent = "ONLINE";
+        document.body.className = "online";
+    } else {
+        statusIcon.textContent = "OFFLINE";
+        document.body.className = "offline";
+    }
+}
+
+window.addEventListener("load", () => {
+    statusIcon = document.querySelector("#status");
+    updateConnected();
+});
+
+window.addEventListener("online", updateConnected);
+window.addEventListener("offline", updateConnected);
